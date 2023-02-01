99e1e326751b6e5f4042895589024a288cdf6567
Andrei Beliakov <andrei@imd.ru>
2023-02-01 14:20:36 +0300

    change footer tag name

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 import { data } from "./data.js";
 const wrap = document.querySelector(".cards__wrapper");
+const search = document.querySelector(".cards__input");
 
 function createCard(obj) {
+  let checkedKeywords = [...new Set(obj.keywords.split(" "))].join(" ");
   const newCard = document.createElement("div");
   newCard.className = "cards__item";
   newCard.innerHTML = `<p class="item__emoji">${obj.symbol}</p> 
-  <p class="item__title">${obj.title}</p>
-  <p class="item__keywords">${obj.keywords}</p>`;
-  wrap.append(newCard);
+     <p class="item__title">${obj.title}</p>
+     <p class="item__keywords">${checkedKeywords}</p>`;
+  return newCard;
 }
 
-data.forEach((card) => createCard(card));
+function searchFunction(event) {
+  wrap.innerHTML = "";
+  console.log(event.target.value);
+  let value = event.target.value.trim().toLowerCase();
+  data
+    .filter(
+      (elem) =>
+        elem.title.trim().toLowerCase().includes(value) ||
+        elem.keywords.trim().toLowerCase().includes(value)
+    )
+    .forEach((elem) => wrap.append(createCard(elem)));
+}
+search.addEventListener("input", searchFunction);
+data.forEach((newCard) => wrap.append(createCard(newCard)));
 
 //
 //
